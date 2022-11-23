4fc0ec3039ab41cee549776d533358563dbfd11f
Johnny Olsen <johnnyolsen90@gmail.com>
2022-11-23 17:51:07 +0100

    Cleaned up run method

--- src/main/java/com/example/mqtt/MessageQueue.java
+++ src/main/java/com/example/mqtt/MessageQueue.java
@@ -35,25 +35,15 @@ public class MessageQueue {
 
     @Scheduled(cron = "0 */2 * * * *")
     public void run() throws MqttException, IOException, InterruptedException {
-        String city = this.cityRepository.findCityById(1L).getCityName();
-        if (city.contains(" ")) {
-            city = city.replaceAll(" ", "%20");
-        }
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.openweathermap.org/data/2.5/weather?q=" + city.trim() + "&appid=34f1dfe47079741556f631b2639de87d"))
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = getWeatherFromWeatherMap();
 
         float temperature = (new JSONObject(response.body()).getJSONObject("main").getBigDecimal("temp").floatValue()) - 273.15f;
         String weatherDescription = StringUtils.capitalize(new JSONObject(response.body()).getJSONArray("weather").getJSONObject(0).getString("description"));
         String cityName = (new JSONObject(response.body()).getString("name"));
-        System.out.println(cityName);
         if (celsius < (temperature - 0.1) || celsius > (temperature + 0.1f)) {
             celsius = temperature;
             String celsiusString = cityName + ": " + String.format("%.2f", celsius) + ";" + weatherDescription;
             String publisherId = UUID.randomUUID().toString();
-
             IMqttClient client = new MqttClient("tcp://83.251.117.120", publisherId, new MqttDefaultFilePersistence("src/main/resources/queue"));
 
             while (!client.isConnected()) {
@@ -63,9 +53,20 @@ public class MessageQueue {
             client.publish("weatherdata/temperature", new MqttMessage(celsiusString.getBytes(StandardCharsets.UTF_8)));
             System.out.println(celsiusString);
 
-        } else {
-            System.out.println("SKIPPED, value was " + String.format("%.2f", temperature));
         }
 
+    }
+
+    private HttpResponse<String> getWeatherFromWeatherMap() throws IOException, InterruptedException {
+        String city = this.cityRepository.findCityById(1L).getCityName();
+        if (city.contains(" ")) {
+            city = city.replaceAll(" ", "%20");
+        }
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.openweathermap.org/data/2.5/weather?q=" + city.trim() + "&appid=34f1dfe47079741556f631b2639de87d"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        return response;
     }
 }
